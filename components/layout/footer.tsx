bc084cd47e0125ccf40badd2f97014ea811e1a00
kalben22 <bernardhoungbo2021@gmail.com>
2026-02-01 05:26:11 +0100

    Link Footer update

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -20,9 +20,9 @@ export default function Footer() {
         </div>
         <p className="text-sm text-moonlight/80">© {new Date().getFullYear()} Association FLOKA. Tous droits réservés.</p>
         <div className="flex items-center gap-4">
-          <SocialIcon href="https://instagram.com/ong" icon={<Instagram className="h-4 w-4" />} label="Instagram" />
-          <SocialIcon href="https://facebook.com/ong" icon={<Facebook className="h-4 w-4" />} label="Facebook" />
-          <SocialIcon href="https://linkedin.com/company/ong" icon={<Linkedin className="h-4 w-4" />} label="LinkedIn" />
+          <SocialIcon href="https://www.instagram.com/fondationfloka?igsh=NDNmbXg0Z25qcG52" icon={<Instagram className="h-4 w-4" />} label="Instagram" />
+          <SocialIcon href="https://www.facebook.com/profile.php?id=100091882151621" icon={<Facebook className="h-4 w-4" />} label="Facebook" />
+          <SocialIcon href="https://www.linkedin.com/in/fondation-floka-4178b9357?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" icon={<Linkedin className="h-4 w-4" />} label="LinkedIn" />
         </div>
       </div>
     </footer>
